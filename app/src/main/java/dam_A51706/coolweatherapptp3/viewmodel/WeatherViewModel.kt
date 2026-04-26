56662be43460709b5fdb9b9fa30ac2edd8827589
MattGon13 <mdgon1304@gmail.com>
2026-04-26 15:37:13 +0100

    Exercício 3.3: FechWeather da camada viewmodel implementada

--- app/src/main/java/dam_A51706/coolweatherapptp3/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/dam_A51706/coolweatherapptp3/viewmodel/WeatherViewModel.kt
@@ -2,18 +2,15 @@ package dam_A51706.coolweatherapptp3.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.google.gson.Gson
-import com.google.maps.android.compose.currentCameraPositionState
-import dam_A51706.coolweatherapptp3.data.WeatherData
+import androidx.lifecycle.viewModelScope
+import dam_A51706.coolweatherapptp3.data.WeatherApiClient
 import dam_A51706.coolweatherapptp3.ui.WeatherUIState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import java.io.InputStreamReader
-import java.net.URL
+import kotlinx.coroutines.launch
+import kotlin.Float
 
 class WeatherViewModel(application: Application): AndroidViewModel(application) {
     private val _weatherUIState= MutableStateFlow(WeatherUIState())
@@ -35,4 +32,25 @@ class WeatherViewModel(application: Application): AndroidViewModel(application) 
         }
     }
 
+    fun fetchWeather() {
+        viewModelScope.launch {
+            val weatherData = WeatherApiClient.getWeather(_weatherUIState.value.latitude, _weatherUIState.value.longitude)
+
+            val current = weatherData?.current_weather
+            val hourly = weatherData?.hourly
+
+            if(weatherData != null && current != null && hourly != null){
+                _weatherUIState.update { currentState ->
+                    currentState.copy(
+                        temperature = current.temperature,
+                        windSpeed = current.windspeed,
+                        windDirection = current.winddirection,
+                        weatherCode = current.weathercode,
+                        seaLevelPressure = hourly.pressure_msl[12].toFloat(),
+                        time = current.time
+                    )
+                }
+            }
+        }
+    }
 }
